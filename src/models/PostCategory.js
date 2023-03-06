b7272bcba8d4bf792e5a1de9bd0ccf7dc3b48541
Miguel <miguelanjohps@gmail.com>
2023-03-06 14:34:10 -0300

    feat: Conclui requisito 11 Cria model PostCategory em src/models/PostCategory.js

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'posts_categories',
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'category',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
