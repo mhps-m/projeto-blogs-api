a562aa3ff2f4604536695d0599405e0841957bff
Miguel <miguelanjohps@gmail.com>
2023-03-06 13:18:37 -0300

    feat: Conclui requisito 07 Cria model Category em src/models/Category.js

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  return Category;
+};
